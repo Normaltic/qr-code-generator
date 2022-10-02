dd54df33d7383e778c51514aae827d2fc7f688c1
Yunji Kim <normaltic@gmail.com>
2022-10-02 20:02:43 +0900

    feat: define section, heading component

---
+++ src/components/Heading3.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Heading3 = styled.h3`
+  font-size: 1.2rem;
+  font-weight: bold;
+`
+
+export default Heading3;
---
+++ src/components/Section.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Section = styled.section`
+  border: 1px solid #DDDDDD;
+  border-radius: 4px;
+`
+
+export default Section;
