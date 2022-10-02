0fca18b2f68f3b554925e4806cf9594ab1ced8ab
Yunji Kim <normaltic@gmail.com>
2022-10-02 17:06:55 +0900

    chore: initialize project

---
+++ src/defaults/resetStyle.tsx
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components";
+
+const ResetStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+    font-size: 16px;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  button {
+    font-size: 16px;
+    outline: none;
+  }
+  input {
+    font-size: 16px;
+    outline: none;
+  }
+`
+
+export default ResetStyle;
---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+import ResetStyle from './defaults/resetStyle';
+
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <ResetStyle />
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
