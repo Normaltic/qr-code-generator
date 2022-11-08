71adf5c0ba3c735fb7e987c9fa331a48d5a64b66
Yunji Kim <normaltic@gmail.com>
2022-11-09 03:11:36 +0900

    refactor: replace resetStyle to component

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import ResetStyle from './defaults/resetStyle';
+import ResetStyle from './components/ResetStyle';
 
 import reportWebVitals from './reportWebVitals';
 
