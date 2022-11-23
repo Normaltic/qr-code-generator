30371839caebb710dbed0b05e72c797c69736306
Yunji Kim <normaltic@gmail.com>
2022-11-24 00:19:00 +0900

    tmp: apply qrcode preview to svg instead of canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import Section from './components/Section';
 import QROptionForm from './components/QROptionForm';
+import QRCanvas from './components/QRCanvas';
 
 function App() {
   return (
-    <div style={{ padding: '15px' }}>
+    <Wrapper>
       <Section>
         <QROptionForm onSubmit={args => console.log(args)} />
       </Section>
-    </div>
+      <Section>
+        <QRCanvas
+          backgroundColor='#FFFFFF'
+          contentColor='#000000'
+          errorCorrectionLevel='L'
+          width={37}
+          link='https://naver.com'
+        />
+      </Section>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 2rem;
+  max-width: 1400px;
+  margin: auto;
+  padding: 0 2rem;
+
+  & > ${Section} {
+    flex: 1;
+  }
+`
